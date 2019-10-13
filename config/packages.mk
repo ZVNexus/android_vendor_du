5c881beab2c39460d47cf4cbddff8035e0ce4e4e
ezio84 <brabus84@gmail.com>
2019-10-13 19:48:16 +0200

    Build prebuilt Pixel Launcher (APK from Google Pixel 3 XL)
    Change-Id: I31b47abfaaceee5b45c6c6277b5de68a2f7f02a6

--- config/packages.mk
+++ config/packages.mk
@@ -1,9 +1,11 @@
 # Packages
 PRODUCT_PACKAGES += \
 #    CustomDoze \
-    WallpaperPicker2
+     WallpaperPicker2
 #    WeatherClient
 
 # Include librsjni explicitly to workaround GMS issue
 PRODUCT_PACKAGES += \
     librsjni
+
+$(call inherit-product, vendor/du/prebuilt/prebuilt.mk)

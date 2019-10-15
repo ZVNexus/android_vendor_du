bf87a4feeb00039297501ea832f7a8936216bf13
ezio84 <brabus84@gmail.com>
2019-10-15 22:10:37 +0200

    Put prebuilt APKs in the product subpartiton.
    Change-Id: I4b5173d4fdda173edaaa96561dfdd875f41c9998

--- prebuilt/priv-app/Android.mk
+++ prebuilt/priv-app/Android.mk
@@ -16,6 +16,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := NexusLauncherRelease
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/$(TARGET_COPY_OUT_PRODUCT)/priv-app
 LOCAL_SRC_FILES := NexusLauncherRelease/NexusLauncherRelease.apk
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_CLASS := APPS
@@ -23,16 +24,19 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Launcher3
 LOCAL_DEX_PREOPT := false
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_PRODUCT_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := WallpaperPickerGooglePrebuilt
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/$(TARGET_COPY_OUT_PRODUCT)/priv-app
 LOCAL_SRC_FILES := WallpaperPickerGooglePrebuilt/WallpaperPickerGooglePrebuilt.apk
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false
+LOCAL_PRODUCT_MODULE := true
 LOCAL_OVERRIDES_PACKAGES := \
     WallpaperPicker \
     WallpaperCropper
